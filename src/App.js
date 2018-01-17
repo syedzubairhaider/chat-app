0ce0cb25bb4041d234b92cb130334e948514d1f2
Zubair Haider <zubair.haider31@gmail.com>
2018-01-17 16:12:18 +0500

    bot1

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     return (
       <div className="container">
         <p className="App-intro">
-        Chat Bot
+        Chat Bot1
         </p>
         <ChatArea  />
       </div>
